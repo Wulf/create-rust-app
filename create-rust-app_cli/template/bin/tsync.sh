ee7d217a3a46cb296b2c6f6e1df5bc24f991c2c0
Haris <4259838+Wulf@users.noreply.github.com>
2022-02-27 05:20:44 -0500

    tsync: Fix script so calling it from anywhere is valid

--- create-rust-app_cli/template/bin/tsync.sh
+++ create-rust-app_cli/template/bin/tsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-tsync -i ../backend -o $SCRIPT_DIR/../frontend/src/types/rust.d.ts
+tsync -i $SCRIPT_DIR/../backend -o $SCRIPT_DIR/../frontend/src/types/rust.d.ts
