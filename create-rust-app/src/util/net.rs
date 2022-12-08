8e4acc0dcb5b54c10882274105d77a3cfb4c7020
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-12-08 00:12:13 -0800

    partial documentation of utils module

--- create-rust-app/src/util/net.rs
+++ create-rust-app/src/util/net.rs
@@ -1,5 +1,6 @@
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6, TcpListener, ToSocketAddrs};
 
+/// binds a [`TcpListener`] to the given [`addr`](`ToSocketAddrs`)
 fn test_bind<A: ToSocketAddrs>(addr: A) -> bool {
     match TcpListener::bind(addr).map(|t| t.local_addr().is_ok()) {
         Ok(result) => result,
@@ -7,6 +8,8 @@ fn test_bind<A: ToSocketAddrs>(addr: A) -> bool {
     }
 }
 
+/// is the given port free 
+/// TODO: free on what server?
 pub fn is_port_free(port: u16) -> bool {
     let ipv4 = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port);
     let ipv6 = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0);
