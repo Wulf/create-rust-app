6d07b200dd98fe9f1cc526a63d57f78f6cccc575
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2023-08-27 01:28:33 -0700

    Hotfix#314 (#315)
    * chore: fmt
    
    * fix: Compilation error after creating the app  #314
    
    * chore: clippy

--- create-rust-app_cli/src/plugins/tasks.rs
+++ create-rust-app_cli/src/plugins/tasks.rs
@@ -55,29 +55,28 @@ impl Plugin for Tasks {
 
         crate::content::migration::create(
             "plugin_tasks",
-            indoc! {r##"
-                CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+            indoc! {r#"CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-                CREATE TYPE fang_task_state AS ENUM ('new', 'in_progress', 'failed', 'finished', 'retried');
+CREATE TYPE fang_task_state AS ENUM ('new', 'in_progress', 'failed', 'finished', 'retried');
 
-                CREATE TABLE fang_tasks (
-                    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-                    metadata jsonb NOT NULL,
-                    error_message TEXT,
-                    state fang_task_state DEFAULT 'new' NOT NULL,
-                    task_type VARCHAR DEFAULT 'common' NOT NULL,
-                    uniq_hash CHAR(64),
-                    retries INTEGER DEFAULT 0 NOT NULL,
-                    scheduled_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-                    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-                    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-                );
+CREATE TABLE fang_tasks (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    metadata jsonb NOT NULL,
+    error_message TEXT,
+    state fang_task_state DEFAULT 'new' NOT NULL,
+    task_type VARCHAR DEFAULT 'common' NOT NULL,
+    uniq_hash CHAR(64),
+    retries INTEGER DEFAULT 0 NOT NULL,
+    scheduled_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
 
-                CREATE INDEX fang_tasks_state_index ON fang_tasks(state);
-                CREATE INDEX fang_tasks_type_index ON fang_tasks(task_type);
-                CREATE INDEX fang_tasks_scheduled_at_index ON fang_tasks(scheduled_at);
-                CREATE INDEX fang_tasks_uniq_hash ON fang_tasks(uniq_hash);
-            "##},
+CREATE INDEX fang_tasks_state_index ON fang_tasks(state);
+CREATE INDEX fang_tasks_type_index ON fang_tasks(task_type);
+CREATE INDEX fang_tasks_scheduled_at_index ON fang_tasks(scheduled_at);
+CREATE INDEX fang_tasks_uniq_hash ON fang_tasks(uniq_hash);
+"#},
             r#"DROP TABLE fang_tasks;"#,
         )?;
 
