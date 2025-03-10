adc20d54b09ea41def00489df12b489a2b15edb3
Haris <4259838+Wulf@users.noreply.github.com>
2025-03-10 05:13:16 -0400

    Removed expired domain: create-rust-app.dev

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,38 +1,38 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Create Rust App',
-  tagline: 'Set up a modern rust+react web app by running one command.',
-  url: 'https://create-rust-app.dev',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Wulf',
-  projectName: 'create-rust-app',
+  title: "Create Rust App",
+  tagline: "Set up a modern rust+react web app by running one command.",
+  url: "https://create-rust-app.dev",
+  baseUrl: "/create-rust-app/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Wulf",
+  projectName: "create-rust-app",
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/Wulf/create-rust-app/edit/main/docs/',
+          editUrl: "https://github.com/Wulf/create-rust-app/edit/main/docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Wulf/create-rust-app/edit/main/docs/blog/',
+            "https://github.com/Wulf/create-rust-app/edit/main/docs/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -42,53 +42,53 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Create Rust App',
+        title: "Create Rust App",
         logo: {
-          alt: 'Create Rust App Logo',
-          src: 'img/cra-logo-rust.svg',
-          srcDark: 'img/cra-logo-rust-white.svg',
+          alt: "Create Rust App Logo",
+          src: "img/cra-logo-rust.svg",
+          srcDark: "img/cra-logo-rust-white.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/Wulf/create-rust-app',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/Wulf/create-rust-app",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Links',
+            title: "Links",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'Docs',
-                to: '/docs/intro',
+                label: "Docs",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/Wulf/create-rust-app',
+                label: "GitHub",
+                href: "https://github.com/Wulf/create-rust-app",
               },
               {
-                label: 'Crates.io',
-                href: 'https://crates.io/crates/create-rust-app',
+                label: "Crates.io",
+                href: "https://crates.io/crates/create-rust-app",
               },
             ],
           },
@@ -99,6 +99,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-}
+};
 
-module.exports = config
+module.exports = config;
